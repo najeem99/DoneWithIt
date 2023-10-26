a0b335b43f5179c351a20616590a6bfbf936da1b
najeem <najeemfasil1@gmail.com>
2023-10-26 15:08:03 +0400

    adding listing screen

---
+++ app/components/Card.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet,Image ,View} from 'react-native';
+import AppText from './AppText';
+import colors from '../config/colors';
+function Card({title,subTitle,image}) {
+    return (
+        <View style={styles.container}>
+        <Image style={styles.imageView} source={image}></Image>
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>
+            {title}
+          </AppText>
+          <AppText style={styles.price}>{subTitle}</AppText>
+        </View>  
+      </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+      borderRadius: 25,
+      overflow: "hidden",
+      justifyContent: "center",
+      alignItems: "flex-start",
+      marginVertical:10
+    },
+    detailsContainer: {
+        width:'100%',
+      backgroundColor:'white',
+      paddingTop: 10,
+      paddingHorizontal: 10,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "600",
+
+    },
+    price: {
+      fontSize: 20,
+      color: colors.secondary,
+      marginVertical: 10,
+    },
+    imageView: {
+      width: "100%",
+      height: 200, 
+      objectFit: "cover",
+    }
+  });
+  
+export default Card;
---
+++ app/screens/ListingScreen.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Screen from "../components/Screen";
+import { FlatList ,StyleSheet} from "react-native";
+import Card from "../components/Card";
+import colors from "../config/colors";
+const listings = [
+  {
+    id: 1,
+    title: "Bicycle with helmet",
+    price: 100,
+    image:
+      "https://www.firefoxbikes.com/on/demandware.static/-/Sites-firefox-navigation/default/dwda09db55/PLPBanner/categoryPLP/MTB/Firefox-Tremor-Web.jpg",
+  },
+  {
+    id: 2,
+    title: "Couch for sale",
+    price: 100,
+    image:
+      "https://livingindubai.org/wp-content/uploads/2016/04/sofa.jpg",
+  },
+  {
+    id: 3,
+    title: "Couch for sale",
+    price: 100,
+    image:
+      "https://livingindubai.org/wp-content/uploads/2016/04/sofa.jpg",
+  },
+];
+function ListingScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) =>  
+          <Card
+            title={item.title}
+            subTitle={"AED " + item.price}
+            image={{ uri: item.image }}
+          />
+         }
+      />
+    </Screen>
+  );
+}
+const styles = StyleSheet.create({
+    screen:{
+        backgroundColor:colors.light,
+        paddingHorizontal:5,
+    }
+})
+export default ListingScreen;
