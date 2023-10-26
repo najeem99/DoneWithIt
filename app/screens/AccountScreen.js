fadb99bc65b18645decb6fb4354ce91d0fc2db06
najeem <najeemfasil1@gmail.com>
2023-10-26 10:29:18 +0400

    accounts screen

---
+++ app/screens/AccountScreen.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import { StyleSheet, View, FlatList } from "react-native";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import Separator from "../components/Separator";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
+
+function AccountScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.constainer}>
+        <ListItem
+          title="John Cena"
+          subTitle="johncena@shiplifier.com"
+          image={{
+            uri: "https://i0.wp.com/www.hausofwrestling.com/wp-content/uploads/2023/08/John-Cena-2.jpeg?fit=866%2C455&ssl=1",
+          }}
+        />
+      </View>
+      <View style={styles.constainer}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+          ItemSeparatorComponent={ <Separator/>}
+        ></FlatList>
+      </View>
+      <ListItem
+          title="Logout"
+          IconComponent={
+            <Icon
+              name="logout"
+              backgroundColor="#ffe66d"
+            />
+          } 
+        />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen:{
+    backgroundColor:colors.light
+  },
+  constainer: {
+    marginVertical: 20,
+  },
+});
+
+export default AccountScreen;
