327e28ac87203850ec56f203e29698a929cbcc7f
najeem <najeemfasil1@gmail.com>
2023-10-31 14:59:25 +0400

    form image picker

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ImageInputList from "../ImageInputList";
+function FormImagePicker({ name }) {
+  const { setFieldValue, handleChange, values, touched, errors } =
+    useFormikContext();
+  const imageUris = values[name];
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+  const handleRemove = (uri) => {
+    console.log(uri);
+    setFieldValue(
+      name,
+      imageUris.filter((imageuri) => imageuri != uri)
+    );
+  };
+  return (
+    <>
+      <ImageInputList
+        imageUris={values[name]}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage visible={touched[name]} error={errors[name]}></ErrorMessage>
+    </>
+  );
+}
+
+export default FormImagePicker;
